9dd3b35e0c2d788da34119807b3ef7dfcc5d6751
Andrew Virts <asvirts@outlook.com>
2023-10-02 20:28:39 -0500

    bg color

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,7 +2,7 @@ import PortfolioItem from "@/components/PortfolioItem";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex container mx-auto min-h-screen items-center justify-between">
       <PortfolioItem />
       <PortfolioItem />
       <PortfolioItem />
--- portfolio/src/components/PortfolioItem.tsx
+++ portfolio/src/components/PortfolioItem.tsx
@@ -1,6 +1,6 @@
 export default function PortfolioItem() {
   return (
-    <div className="columns-3">
+    <div className="bg-[#f1f5f9] columns-auto p-12 rounded">
       <h2>Test</h2>
     </div>
   );
