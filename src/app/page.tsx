8b797f43e33995de69ad89fa81a542919bdc5a6a
Andrew Virts <asvirts@outlook.com>
2023-10-10 17:31:40 -0500

    portfolio items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-import PortfolioItem from "@/components/PortfolioItem";
-
 export default function Home() {
   return (
-    <main className="flex flex-wrap container mx-auto min-h-screen items-center justify-between">
-      <PortfolioItem
-        title="SpaceX"
-        desc="Practicing querying an API with GraphQL in React."
-      />
-      <PortfolioItem
-        title="Onyxia's Lair"
-        desc="This is a text-based JavaScript game."
-      />
-      <PortfolioItem title="Madlib" desc="Madlib game written in C#." />
-      <PortfolioItem title="Calculator" desc="React calulator" />
+    <main className="flex flex-wrap container mx-auto min-h-screen items-center justify-center">
+      <h1>Andrew Virts</h1>
     </main>
   );
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,18 @@
+import PortfolioItem from "@/components/PortfolioItem";
+
 export default function Portfolio() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            Portfolio page
-        </main>
-        )
+  return (
+    <main className="flex flex-wrap container mx-auto min-h-screen items-center justify-between">
+      <PortfolioItem
+        title="SpaceX"
+        desc="Practicing querying an API with GraphQL in React."
+      />
+      <PortfolioItem
+        title="Onyxia's Lair"
+        desc="This is a text-based JavaScript game."
+      />
+      <PortfolioItem title="Madlib" desc="Madlib game written in C#." />
+      <PortfolioItem title="Calculator" desc="React calulator" />
+    </main>
+  );
 }
