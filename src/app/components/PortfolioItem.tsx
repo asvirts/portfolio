8b0e790510be23157dac3e4f4ba2d424635837e7
Andrew Virts <asvirts@outlook.com>
2023-10-11 18:52:52 -0500

    props type

--- src/app/components/PortfolioItem.tsx
+++ src/app/components/PortfolioItem.tsx
@@ -1,4 +1,4 @@
-export default function PortfolioItem(props) {
+export default function PortfolioItem(props: any) {
   return (
     <div className="bg-[#f1f5f9] flex-wrap columns-auto mx-1 w-1/4 p-12 rounded text-center">
       <h2 className="font-bold">{props.title}</h2>
