600c481a24437999edf9d48a69ff4cf7680109f3
Andrew Virts <asvirts@outlook.com>
2023-10-11 19:25:17 -0500

    home page image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex flex-wrap container mx-auto min-h-screen items-center justify-center">
-      <h1>Andrew Virts</h1>
+    <main className="flex flex-wrap container mx-auto min-h-screen items-start justify-center mt-10">
+      <Image
+        src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2607&q=80"
+        width={800}
+        height={600}
+        alt="Picture of the author"
+      />
     </main>
   );
 }
