39aa8854f94a42173989f3d5c5b0d5fff4484601
Andrew Virts <asvirts@outlook.com>
2023-10-11 19:15:20 -0500

    created about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,33 @@
+/* eslint-disable react/no-unescaped-entities */
 export default function About() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            About page
-        </main>
-        )
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="container md:w-2/6 sm:w-full">
+        <h1 className="font-bold text-2xl mb-3 block">
+          Software Engineer on the Bayou
+        </h1>
+        <p className="my-3">
+          A curious learner and passionate creator sharing my journey with the
+          world. Using my skills and passions to make a positive impact and
+          empower others to do the same.
+        </p>
+        <p className="my-3">
+          I am driven by a passion for creating innovative solutions to
+          real-world problems. I'm always on the lookout for new, challenging
+          projects that allow me to continue learning and growing in this field.
+        </p>
+        <p className="my-3">
+          I have had the opportunity to work on a diverse range of projects,
+          from small business websites to large-scale web applications. I am
+          highly collaborative and enjoy working with a team, but am also
+          comfortable taking on individual projects and seeing them through to
+          completion.
+        </p>
+        <p className="my-3">
+          When I'm not coding, you'll likely find me hanging out with my family,
+          playing video games, or watching sports.
+        </p>
+      </div>
+    </main>
+  );
 }
