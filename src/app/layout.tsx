8a3f1432348963b93bbda04d237c0979640cfa5e
Andrew Virts <asvirts@outlook.com>
2023-10-14 08:48:11 -0500

    contact me link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a
-              href="#"
+              href="mailto:andrewvirts@gmail.com"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               Contact me <span aria-hidden="true">&rarr;</span>
