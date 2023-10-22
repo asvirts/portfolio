aa76d5f1d41630c65ee9804092d1929f9889017c
Andrew Virts <asvirts@outlook.com>
2023-10-22 13:29:25 -0500

    new home page

--- src/app/components/PortfolioItem.tsx
+++ src/app/components/PortfolioItem.tsx
@@ -2,26 +2,28 @@ import Link from "next/link";
 
 export default function PortfolioItem(props: any) {
   return (
-    <div className="card w-1/4 bg-base-100 shadow-xl m-2">
-      <figure>
-        <img src={props.img} alt={props.title} className="portfolio-item" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{props.title}</h2>
-        <p>{props.desc}</p>
-        <small className="text-slate-400">
-          <strong>Techstack:</strong> {props.tech}
-        </small>
-        <div className="card-actions justify-end">
-          <Link
-            href={`${props.link}`}
-            target="_blank"
-            className="btn btn-primary"
-          >
-            View on GitHub
-          </Link>
-        </div>
+    <a
+      href={props.link}
+      className="group block overflow-hidden lg:w-1/4 md:w-1/3 sm:w-full mx-2 my-4"
+    >
+      <div className="relative h-[350px] sm:h-[450px]">
+        <img
+          src={props.img}
+          alt={props.title}
+          className="absolute inset-0 h-full w-full object-cover opacity-100"
+        />
       </div>
-    </div>
+
+      <div className="relative pt-3">
+        <h3 className="text-sm text-gray-700 group-hover:underline group-hover:underline-offset-4">
+          {props.title}
+        </h3>
+
+        <p className="mt-1.5 tracking-wide text-gray-900">{props.desc}</p>
+        <small className="text-gray-400">
+          <strong>Tech Stack:</strong> {props.tech}
+        </small>
+      </div>
+    </a>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,40 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-wrap container mx-auto min-h-screen items-start justify-center mt-10">
-      <Image
-        src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2607&q=80"
-        width={800}
-        height={600}
-        alt="Picture of the author"
-      />
+      <section>
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+            <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+              <Image
+                src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2607&q=80"
+                width={800}
+                height={600}
+                alt="Do something great"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
+            </div>
+
+            <div className="lg:py-24">
+              <h2 className="text-3xl font-bold sm:text-4xl">
+                Your story. My medium.
+              </h2>
+
+              <p className="mt-4 text-gray-600">
+                A curious learner and passionate creator sharing my journey with
+                the world. Using my skills and passions to make a positive
+                impact and empower others to do the same.
+              </p>
+
+              <a
+                href="/portfolio"
+                className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+              >
+                See my work
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
