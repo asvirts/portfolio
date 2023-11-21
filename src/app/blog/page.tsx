9002d17c6636eb219c72e50e5db442578e21fcbe
Andrew Virts <asvirts@outlook.com>
2023-11-21 16:49:24 -0600

    blog page update

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,7 @@
-import GetAllBlogPosts from "../components/GetAllBlogPosts";
-
 export default function Blog() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <GetAllBlogPosts />
+      <p>coming soon...</p>
     </main>
-  );
+  )
 }
