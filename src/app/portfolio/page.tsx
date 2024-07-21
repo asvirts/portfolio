1de4f9e9624dc1554c0109a903a77037fbc9626f
Andrew Virts <asvirts@outlook.com>
2024-07-21 15:27:39 -0500

    portfolio item image updates

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -9,14 +9,14 @@ export default function Portfolio() {
           desc="Unlock Your Focus, One Pomodoro at a Time."
           tech="Flutter, Dart, Riverpod, "
           link="https://github.com/asvirts/pomodoro"
-          img="https://camo.githubusercontent.com/3de310c47ce0aff7fd290cac6652f68bd14a40f8b9a42d43c03bb8fef936a452/68747470733a2f2f6d656469612e6c6963646e2e636f6d2f646d732f696d6167652f433445313241514547397a62586136523159772f61727469636c652d636f7665725f696d6167652d736872696e6b5f3732305f313238302f302f313536373437323539313530363f653d3231343734383336343726763d6265746126743d734776575a4f63415856376a444d6d654144315f7a766946483972316e722d735a59597a70306357776d34"
+          img="https://t4.ftcdn.net/jpg/08/29/52/97/360_F_829529710_3GtyPgMV8wUjd7lpik1C7iZ9f8OFR84A.jpg"
         />
         <PortfolioItem
           title="Giphy Search"
           desc="Find your next favorite GIF... the cool way"
           tech="React, Next.js, Typescript, APIs, Tailwind"
           link="https://github.com/asvirts/giphy-search"
-          img="https://camo.githubusercontent.com/1e9481a8186553af39b570e642d047bd4574b50280c6ea1890cd9c9dfc760629/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f3145543768526c43635a4c75452f67697068792e676966"
+          img="https://blog.hootsuite.com/wp-content/uploads/2022/06/How-to-Make-a-GIF-1.gif"
         />
         <PortfolioItem
           title="TaskWallet"
@@ -29,7 +29,8 @@ export default function Portfolio() {
           title="Onyxia's Lair"
           desc="(Mostly) text-based browser game."
           tech="Vanilla JavaScript, CSS"
-          link="https://i.pinimg.com/originals/19/f1/ca/19f1ca5691f3f85b4f105463dce89fa5.jpg"
+          link="https://github.com/asvirts/onyxias-lair"
+          img="https://img2.wallspic.com/previews/0/7/7/1/5/151770/151770-andorhal-world_of_warcraft-onyxia-ragnaros-wow_classic-x750.jpg"
         />
         <PortfolioItem
           title="Madlib"
